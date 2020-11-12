b60335fe0e16342a21ec14c97d74012f9459fbb3
artidex <artidex028@gmail.com>
2020-11-12 11:35:43 +0200

    fix port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(function(req, res, next) {
     res.status(404).send('Sorry cant find that!');
   });
 
-const port = process.env.APP_PORT 
+const port = process.env.PORT 
 
 app.listen(port || 8080, (error) => {
     console.log('Server started', new Date())
