db762067adc2c4af94d61cd2d8ca4806cc908570
artidex <artidex028@gmail.com>
2020-11-12 10:06:42 +0200

    back env variables

--- configDB.js
+++ configDB.js
@@ -2,20 +2,13 @@ const mysql = require('mysql')
 const dotenv = require('dotenv')
 dotenv.config()
 
-// const pool = mysql.createPool({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     connectionLimit: 10
-// })
-
 const pool = mysql.createPool({
-    host: 'us-cdbr-east-02.cleardb.com',
-    user: 'bd419084d34966',
-    password: '94bd8da7',
-    database: 'heroku_737034524489d85',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectionLimit: 10
 })
+
 
 module.exports = pool;
